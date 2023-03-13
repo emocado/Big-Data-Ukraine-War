d2e12c254c8f5b98e54e9ab00267cdec330b1c50
Wei Khiang <weikhiang92000@gmail.com>
2023-03-14 01:04:30 +0800

    lambda_function works on AWS

---
+++ twitter/lambda_function.py
@@ -0,0 +1,39 @@
+import json
+import urllib.parse
+import boto3
+import snscrape.modules.twitter as sntwitter
+from datetime import datetime, timedelta
+
+print('Loading function')
+
+s3 = boto3.client('s3')
+
+
+def lambda_handler(event, context):
+    key=f"{datetime.utcnow().replace(second=0, microsecond=0)}.json"
+    bucket="wklee-is459"
+    query = "ukraine war"
+    try:
+        # Set the start and end dates for the search (in UTC timezone)
+        start_date = datetime.utcnow()
+        end_date = start_date.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=1)
+        tweets = []
+        for i, tweet in enumerate(sntwitter.TwitterSearchScraper(f"{query} since:{start_date.date()} until:{end_date.date()}").get_items()):
+            if tweet.date.time() < (start_date - timedelta(minutes=15)).time():
+                break
+            tweets.append({
+                'id': tweet.id,
+                'date': tweet.date.strftime('%Y-%m-%d %H:%M:%S'),
+                'content': tweet.rawContent,
+                "username": tweet.user.username,
+                "followersCount": tweet.user.followersCount,
+                "mentionedUsers": [user.username for user in tweet.mentionedUsers] if tweet.mentionedUsers else None,
+                "retweetCount": tweet.retweetCount,
+                "replyCount": tweet.replyCount,
+                "inReplyToUser": tweet.inReplyToUser.username if tweet.inReplyToUser else None,
+            })
+        tweet_json = json.dumps(tweets)
+        response = s3.put_object(Body=tweet_json, Bucket=bucket, Key=key)
+        return response
+    except Exception as e:
+        print(e)
--- twitter/twitter_scrape.py
+++ twitter/twitter_scrape.py
@@ -1,17 +1,31 @@
+import json
 import snscrape.modules.twitter as sntwitter
 from datetime import datetime, timedelta
 
-
 query = "ukraine war"
 # Set the start and end dates for the search (in UTC timezone)
-end_date = datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)
-start_date = end_date - timedelta(days=1)
+start_date = datetime.utcnow()
+end_date = start_date.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=1)
 tweets = []
 for i, tweet in enumerate(sntwitter.TwitterSearchScraper(f"{query} since:{start_date.date()} until:{end_date.date()}").get_items()):
+    if tweet.date.time() < (start_date - timedelta(minutes=15)).time():
+        break
     tweets.append({
         'id': tweet.id,
         'date': tweet.date.strftime('%Y-%m-%d %H:%M:%S'),
-        'content': tweet.rawContent
-    })
+        'content': tweet.rawContent,
+        "username": tweet.user.username,
+        "followersCount": tweet.user.followersCount,
+        "mentionedUsers": [user.username for user in tweet.mentionedUsers] if tweet.mentionedUsers else None,
+        "retweetCount": tweet.retweetCount,
+        "replyCount": tweet.replyCount,
+        "inReplyToUser": tweet.inReplyToUser.username if tweet.inReplyToUser else None,
+        })
+    
+with open (f"twitter/twitter_dump.json", "w") as f:
+    json.dump(tweets, f)
 
-print(tweets)
+
+# print(tweets)
+print(len(tweets))
+
