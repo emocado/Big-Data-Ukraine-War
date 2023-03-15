214282fc3d91e9f68855f9c43157906c868acd38
Wei Khiang <weikhiang92000@gmail.com>
2023-03-16 00:23:21 +0800

    update twitter  to include timestamp of scrape

--- twitter/lambda_function.py
+++ twitter/lambda_function.py
@@ -10,13 +10,14 @@ s3 = boto3.client('s3')
 
 
 def lambda_handler(event, context):
-    key=f"{datetime.utcnow().replace(second=0, microsecond=0)}.json"
+    # Set the start and end dates for the search (in UTC timezone)
+    start_date = datetime.utcnow()
+    time_stamp = start_date.replace(second=0, microsecond=0)
+    end_date = start_date.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=1)
+    key=f"project/{time_stamp}.json"
     bucket="wklee-is459"
     query = "ukraine war"
     try:
-        # Set the start and end dates for the search (in UTC timezone)
-        start_date = datetime.utcnow()
-        end_date = start_date.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=1)
         tweets = []
         for i, tweet in enumerate(sntwitter.TwitterSearchScraper(f"{query} since:{start_date.date()} until:{end_date.date()}").get_items()):
             if tweet.date.time() < (start_date - timedelta(minutes=15)).time():
@@ -31,6 +32,7 @@ def lambda_handler(event, context):
                 "retweetCount": tweet.retweetCount,
                 "replyCount": tweet.replyCount,
                 "inReplyToUser": tweet.inReplyToUser.username if tweet.inReplyToUser else None,
+                "timeStamp": str(time_stamp)
             })
         tweet_json = json.dumps(tweets)
         response = s3.put_object(Body=tweet_json, Bucket=bucket, Key=key)
