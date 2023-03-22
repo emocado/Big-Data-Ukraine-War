62feb9b8c241d357838eadceac41bfd7ae3ac4da
leeshuoan <leeshuoan38@gmail.com>
2023-03-22 12:42:07 +0800

    updated neo4j insert script for reddit

--- neo4j_insert/neo4j_reddit_insert.py
+++ neo4j_insert/neo4j_reddit_insert.py
@@ -5,28 +5,36 @@ import json
 import os
 
 
-def create_post(tx, id, date, title, content, username, commentCount, score, subreddit):
+def create_post_relationships(tx, id, date, title, content, username, commentCount, score, subreddit):
     query = (
-        "CREATE (p1:Post { id: $id, date: $date, title: $title, content: $content, username: $username, commentCount: $commentCount, score: $score, subreddit: $subreddit })"
-        "RETURN p1"
+        "MERGE (p1:Post { id: $id, date: $date, title: $title, content: $content, username: $username, commentCount: $commentCount, score: $score, subreddit: $subreddit })"
+        "MERGE (r1:Subreddit { name: $subreddit })"
+        "MERGE (u1:User { username: $username })"
+        "MERGE (p1)-[:POSTED_IN]->(r1)"
+        "MERGE (p1)-[:POSTED_BY]->(u1)"
+        "RETURN p1, r1, u1"
     )
     result = tx.run(query, id=id, date=date, title=title, content=content, username=username, commentCount=commentCount, score=score, subreddit=subreddit)
     try:
-        return [{"p1": row["p1"]["id"]} for row in result]
+        return [{"p1": row["p1"]["id"], "r1": row["r1"]["name"], "u1": row["u1"]["username"]} for row in result]
     except ServiceUnavailable as exception:
         print("{query} raised an error: \n {exception}".format(
             query=query, exception=exception))
         raise
     
 
-def create_comment(tx, id, date, content, username, score, post_id):
+def create_comment_relationships(tx, id, date, content, username, score, post_id):
     query = (
-        "CREATE (c1:Comment { id: $id, date: $date, content: $content, username: $username, score: $score, postId: $postId })"
-        "RETURN c1"
+        "MERGE (p1:Post { id: $postId })"
+        "MERGE (u1:User { username: $username })"
+        "MERGE (c1:Comment { id: $id, date: $date, content: $content, username: $username, score: $score, postId: $postId })"
+        "MERGE (c1)-[:COMMENTED_ON]->(p1)"
+        "MERGE (c1)-[:COMMENTED_BY]->(u1)"
+        "RETURN c1, u1"
     )
     result = tx.run(query, id=id, date=date, content=content, username=username, score=score, postId=post_id)
     try:
-        return [{"c1": row["c1"]["id"]} for row in result]
+        return [{"c1": row["c1"]["id"], "u1": row["u1"]["username"]} for row in result]
     except ServiceUnavailable as exception:
         print("{query} raised an error: \n {exception}".format(
             query=query, exception=exception))
@@ -37,15 +45,12 @@ def create_orchestrator(uri, user, password, posts, commetns):
     data_base_connection = GraphDatabase.driver(uri=uri, auth=(user, password))
     with data_base_connection.session(database="neo4j") as session:
         # Write transactions allow the driver to handle retries and transient errors
-        print(session)
         for post in posts:
-            result = session.execute_write(create_post, post["id"], post["date"], post["title"], post["content"], post["username"], post["commentCount"], post["score"], post["subreddit"])
-            for row in result:
-                print(f"Created: {result}")
+            result = session.execute_write(create_post_relationships, post["id"], post["date"], post["title"], post["content"], post["username"], post["commentCount"], post["score"], post["subreddit"])
+            print(f"Created: {result}")
         for comment in comments:
-            result = session.execute_write(create_comment, comment["id"], comment["date"], comment["content"], comment["username"], comment["score"], comment["post_id"])
-            for row in result:
-                print(f"Created: {result}")
+            result = session.execute_write(create_comment_relationships, comment["id"], comment["date"], comment["content"], comment["username"], comment["score"], comment["post_id"])
+            print(f"Created: {result}")
 
 
 def delete_all(tx):
@@ -78,10 +83,7 @@ if __name__ == "__main__":
     with open('reddit_comments_dump.json', 'r') as outfile:
         comments = json.load(outfile)
     uri = os.environ.get("NEO4J_URI")
-    print(uri)
     user = os.environ.get("NEO4J_USER")
-    print(user)
     password = os.environ.get("NEO4J_PASSWORD")
-    print(password)
     delete_database(uri, user, password)
     create_orchestrator(uri, user, password, posts, comments)
