82fc6b76899308c3dc17ad9e2b5b9710010143ed
jowannyeak <50290926+jowannyeak@users.noreply.github.com>
2023-03-29 19:00:46 +0800

    updated reddit aggregate script to exclude posts that are no longer available

--- reddit/lambda_function_aggregate.py
+++ reddit/lambda_function_aggregate.py
@@ -37,22 +37,35 @@ def lambda_handler(event, context):
     topics = obj['Body'].read().decode("utf-8").split("\n")
     
     for query in topics:
+        updated_posts = []
         posts = []
         comments = []
         bucket_search_content = s3.list_objects_v2(Bucket=bucket, Prefix=f"reddit_initial/topic={query}/dataload={crawl_day}/").get('Contents', [])
+        # bucket_search_content = s3.list_objects_v2(Bucket=bucket, Prefix=f"reddit_initial/topic={query}/dataload=27-03-2023/").get('Contents', [])
+
         file_keys = [file['Key'] for file in bucket_search_content]
         for key in file_keys:
             post_file = s3.get_object(Bucket=bucket, Key=key)
             file_content = json.loads((post_file['Body'].read().decode("utf-8")))
             posts.extend(file_content)
     
-        try:
-            posts_key=f"project/reddit_posts/topic={query}/dataload={recrawl_day}/{time_stamp}_posts_aggregated.json"
-            comments_key=f"project/reddit_comments/topic={query}/dataload={recrawl_day}/{time_stamp}_comments.json"
-            for post in posts:
+        posts_key=f"project/reddit_posts/topic={query}/dataload={recrawl_day}/{time_stamp}_posts_aggregated.json"
+        comments_key=f"project/reddit_comments/topic={query}/dataload={recrawl_day}/{time_stamp}_comments.json"
+        # posts_key=f"project/reddit_posts_aggregated_test.json"
+        # comments_key=f"project/reddit_comments_test.json"
+        for post in posts:
+            try:
                 updated_post = reddit.submission(id=post['id'])
-                post['commentCount'] = int(updated_post.num_comments)
-                post['score'] = int(updated_post.score)
+                updated_posts.append({
+                    'id':str(updated_post.id),
+                    'date': str(datetime.fromtimestamp(updated_post.created_utc)),
+                    'title':str(updated_post.title),
+                    'content':str(updated_post.selftext),
+                    'username':str(updated_post.author),
+                    'subreddit':str(updated_post.subreddit),
+                    'commentCount': int(updated_post.num_comments),
+                    'score': int(updated_post.score),
+                })
                 if updated_post.num_comments > 0:
                     submission = reddit.submission(id=post['id'])
                     submission.comments.replace_more(limit=None)
@@ -70,10 +83,10 @@ def lambda_handler(event, context):
                             'post_id': str(updated_post.id),
                             'parent_id': str(comment.parent_id),
                         })
-        except Exception as e:
-            print(e)
+            except Exception as e:
+                print(e)
             
-        posts_json = json.dumps(posts)
+        posts_json = json.dumps(updated_posts)
         comments_json = json.dumps(comments)
         response1 = s3.put_object(Body=posts_json, Bucket=bucket, Key=posts_key)
         response2 = s3.put_object(Body=comments_json, Bucket=bucket, Key=comments_key)        
--- reddit/reddit_scrape_aggregate.py
+++ reddit/reddit_scrape_aggregate.py
@@ -12,50 +12,58 @@ reddit = praw.Reddit(
     user_agent="IS459-BigData/1.0.0"
 )
 
-posts = []
+updated_posts = []
 comments = []
 post_date = datetime.utcnow() - timedelta(days=3)
 # post_date = datetime.utcnow() - timedelta(minutes=15)
 time_stamp = datetime.utcnow().replace(second=0, microsecond=0)
 
 # post_files = fnmatch.filter(os.listdir('reddit/'), str(post_date)[:10]+'*_posts.json')
-post_files = ['reddit_posts_dump.json']
+post_files = ['2023-03-27 07-56-00.json']
 
 for file in post_files:
     with open('reddit/'+file) as f:
-        posts.extend(json.load(f))
+        posts = json.load(f)
 
-for post in posts:
-    try:
-        updated_post = reddit.submission(id=post['id'])
-        post['commentCount'] = int(updated_post.num_comments)
-        post['score'] = int(updated_post.score)
-        if updated_post.num_comments > 0:
-            submission = reddit.submission(id=post['id'])
-            submission.comments.replace_more(limit=None)
-            for comment in submission.comments.list():
-                if str(comment.author) == "AutoModerator":
-                    continue
-                if comment.author == None:
-                    continue
-                comments.append({
-                    'id': str(comment.id),
-                    'date': str(datetime.fromtimestamp(comment.created_utc)),
-                    'content': str(comment.body),
-                    'username': str(comment.author.name),
-                    'score': int(comment.score),
-                    'post_id': str(updated_post.id),
-                    'parent_id': str(comment.parent_id),
-                })
-    except Exception as e:
-        print("Error: " + str(updated_post.id) + ", " + str(comment.id))
-        print(e)
-        continue
+    for post in posts:
+        try:
+            updated_post = reddit.submission(id=post['id'])
+            updated_posts.append({
+                'id':str(updated_post.id),
+                'date': str(datetime.fromtimestamp(updated_post.created_utc)),
+                'title':str(updated_post.title),
+                'content':str(updated_post.selftext),
+                'username':str(updated_post.author),
+                'subreddit':str(updated_post.subreddit),
+                'commentCount': int(updated_post.num_comments),
+                'score': int(updated_post.score),
+            })
+            if updated_post.num_comments > 0:
+                submission = reddit.submission(id=updated_post['id'])
+                submission.comments.replace_more(limit=None)
+                for comment in submission.comments.list():
+                    if str(comment.author) == "AutoModerator":
+                        continue
+                    if comment.author == None:
+                        continue
+                    comments.append({
+                        'id': str(comment.id),
+                        'date': str(datetime.fromtimestamp(comment.created_utc)),
+                        'content': str(comment.body),
+                        'username': str(comment.author.name),
+                        'score': int(comment.score),
+                        'post_id': str(updated_post.id),
+                        'parent_id': str(comment.parent_id),
+                    })
+        except Exception as e:
+            # print("Error: " + str(updated_post.id) + ", " + str(comment.id))
+            print(e)
+            continue
             
 with open (f"reddit/{time_stamp}_posts_aggregated.json", "w") as f:
-    json.dump(posts, f, ensure_ascii=False)
+    json.dump(updated_posts, f, ensure_ascii=False)
 with open (f"reddit/{time_stamp}_comments.json", "w") as f:
     json.dump(comments, f, ensure_ascii=False)
 
-print(len(posts))
+print(len(updated_posts))
 print(len(comments))
